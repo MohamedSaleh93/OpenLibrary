43a0c84f552aa754d38f5284ff46adafb7ef694b
mohamed.saleh <mohamed_andela@medable.com>
2022-01-17 14:12:59 +0200

    build the UI for main activity and for result screen

---
+++ app/src/main/java/com/mohamed/opendocumentlibrary/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.mohamed.opendocumentlibrary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsActivity: AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_details)
+	}
+}
--- app/src/main/java/com/mohamed/opendocumentlibrary/MainActivity.kt
+++ app/src/main/java/com/mohamed/opendocumentlibrary/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.mohamed.opendocumentlibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+		menuInflater.inflate(R.menu.options_menu, menu)
+		val search = menu.findItem(R.id.search)
+		val searchView = search.actionView as SearchView
+		searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+			override fun onQueryTextSubmit(p0: String?): Boolean {
+				Toast.makeText(this@MainActivity, p0, Toast.LENGTH_LONG).show()
+				return false
+			}
+
+			override fun onQueryTextChange(p0: String?): Boolean {
+				return false
+			}
+
+		})
+		return true
 	}
 }
