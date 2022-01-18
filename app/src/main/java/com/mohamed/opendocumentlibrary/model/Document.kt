2909aeb0165633077db9fa9d6e67f28ab0b37aa9
mohamed.saleh <mohamed_andela@medable.com>
2022-01-18 17:38:42 +0200

    build all the model class

---
+++ app/src/main/java/com/mohamed/opendocumentlibrary/model/Document.kt
@@ -0,0 +1,7 @@
+package com.mohamed.opendocumentlibrary.model
+
+data class Document(
+	val title: String,
+	val isbn: List<String>,
+	val author: String
+)
---
+++ app/src/main/java/com/mohamed/opendocumentlibrary/viewstate/DocumentsListViewState.kt
@@ -0,0 +1,11 @@
+package com.mohamed.opendocumentlibrary.viewstate
+
+import com.mohamed.opendocumentlibrary.model.Document
+
+sealed class DocumentsListViewState
+
+class DocumentsDataListResult(val documents: List<Document>): DocumentsListViewState()
+
+class ErrorState(val errorMessage: String?): DocumentsListViewState()
+
+object LoadingState: DocumentsListViewState()
