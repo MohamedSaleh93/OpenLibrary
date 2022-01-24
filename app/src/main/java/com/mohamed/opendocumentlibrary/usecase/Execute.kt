4d0851351d75c2c84afd63f5d246d5861bad730a
MohamedSaleh93 <mohamedsaleh.fcis@gmail.com>
2022-01-24 12:04:25 +0200

    remove unused class

--- app/src/main/java/com/mohamed/opendocumentlibrary/usecase/Execute.kt
+++
@@ -1,9 +0,0 @@
-package com.mohamed.opendocumentlibrary.usecase
-
-import com.mohamed.opendocumentlibrary.model.Document
-
-interface Execute {
-	fun onSuccess(result: List<Document>)
-
-	fun onError(ex: Exception)
-}
